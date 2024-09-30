2601dad0feb60756f067b65d98db0fc96b8165f6
kuk329 <kuk329@cau.ac.kr>
2024-09-30 14:55:35 +0900

    feat: 헤더

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -1,10 +1,39 @@
 <template>
-    <div>
-            <h1>Header</h1>
+    <header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Sales Boost</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto">
+
+        <li class="nav-item">
+          <RouterLink class="nav-link" active-class="active" aria-current="page" to="/">메인</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" active-class="active" to="/calendar">캘린더</RouterLink>
+        </li>
+
+
+      </ul>
+      <div class="d-flex" role="search">
+        <button class="btn btn-outline-light" type="button" @click="goLoginPage">로그인</button>
+      </div>
     </div>
+  </div>
+</nav>
+  </header>
 </template>
 
 <script setup>
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
+    const goLoginPage=()=>{
+        router.push("/login");
+    }
 
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import App from './App.vue'
 import router from './router/router'
 import { createPinia } from 'pinia'
 
+// bootstrap 사용
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 const pinia = createPinia();
 
 createApp(App).use(router).use(pinia).mount('#app')
