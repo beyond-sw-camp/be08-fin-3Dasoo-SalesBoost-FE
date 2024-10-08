df5efd75e3bce76c77887df5963fa191f52360d5
uzz99 <yj.lee991228@gmail.com>
2024-10-08 15:34:22 +0900

    fix: #19 plan 시간 설정 및 관련 오류 수정
    - starttime, endtime 속성 추가
    - 시간 기반 일정이 표시되도록 설정 변경
    - save 및 close 버튼 클릭 시 입력 값 초기화
    - close, 부모 컴포넌트에 직접 접근하는 방식 대신 메서드 호출 방식으로 변경
    - 불필요한 주석 제거

--- src/components/modal/PlanModal.vue
+++ src/components/modal/PlanModal.vue
@@ -3,8 +3,8 @@
 		<v-card>
 			<v-card-title>일정 추가</v-card-title>
 			<v-card-text>
-				<v-form @submit.prevent="addPlan">
-					<v-alert v-if="showAlert" type="warning" class="toast-alert">
+				<v-form @submit.prevent="submitPlan">
+					<v-alert v-if="showAlert" type="warning" class="warn-alert">
 						필수 입력 필드를 모두 채워주세요.
 					</v-alert>
 					<v-row>
@@ -51,8 +51,8 @@
 			</v-card-text>
 			<v-card-actions>
 				<v-spacer></v-spacer>
-				<v-btn color="close" @click="$emit('close')">Close</v-btn>
-				<v-btn color="success" variant="text" @click="addPlan" flat>Save</v-btn>
+				<v-btn color="close" @click="closeModal">Close</v-btn>
+				<v-btn color="success" variant="text" @click="submitPlan" flat>Save</v-btn>
 			</v-card-actions>
 		</v-card>
 	</v-dialog>
@@ -77,7 +77,7 @@ export default {
     generateTimeOptions() {
       const options = [];
       for (let hour = 0; hour < 24; hour++) {
-        for (let minute = 0; minute < 60; minute += 30) { // 30분 간격으로 시간 생성
+        for (let minute = 0; minute < 60; minute += 30) {
           const paddedHour = hour.toString().padStart(2, '0');
           const paddedMinute = minute.toString().padStart(2, '0');
           options.push(`${paddedHour}:${paddedMinute}`);
@@ -85,7 +85,7 @@ export default {
       }
       return options;
     },
-		addPlan() {
+    submitPlan() {
 			if (!this.plan.title || !this.plan.planCls || !this.plan.planDate || !this.plan.startTime || !this.plan.endTime) {
 				this.showAlert = true;
 				setTimeout(() => {
@@ -94,19 +94,23 @@ export default {
 				return;
 			}
 			this.showAlert = false;
-			this.$emit('add');
+      this.$emit('add', this.plan);
+      this.closeModal();
+		},
+		closeModal() {
+			this.$emit('close');
 		},
 	},
 };
 </script>
 
 <style scoped>
-.toast-alert {
+.warn-alert {
   position: fixed;
-  bottom: 10%; /* 원하는 위치에 맞게 수정 가능 */
+  bottom: 10%; 
   left: 50%;
   transform: translateX(-50%);
-  z-index: 3000; /* 모달 위에 뜨도록 높은 값 설정 */
-  width: 100%; /* 필요에 따라 너비 조정 */
+  z-index: 3000;
+  width: 100%;
 }
 </style>
