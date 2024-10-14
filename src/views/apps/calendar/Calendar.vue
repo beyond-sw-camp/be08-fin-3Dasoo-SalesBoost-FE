8fd66ed1cf0a179d2488f82d21a025f382ba4310
uzz99 <yj.lee991228@gmail.com>
2024-10-14 11:04:14 +0900

    refact: #29 사용하지 않는 코드 정리

--- src/views/apps/calendar/Calendar.vue
+++ src/views/apps/calendar/Calendar.vue
@@ -5,28 +5,12 @@ import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
 import UiParentCard from '@/components/shared/UiParentCard.vue';
 import FullCalendar from './FullCalender.vue';
 import CalendarFilters from './CalendarFilters.vue';
-const date = ref(new Date());
-const seldate = ref(new Date('2018-03-02'))
 const adapter = useDate()
 
 function allowedDates(val: unknown) {
     return parseInt(adapter.toISO(val).split('-')[2], 10) % 2 === 0
 }
 
-
-const page = ref({ title: 'Calendar' });
-const breadcrumbs = ref([
-    {
-        text: 'Dashboard',
-        disabled: false,
-        href: '#'
-    },
-    {
-        text: 'Calendar',
-        disabled: true,
-        href: '#'
-    }
-]);
 </script>
 
 <template>
