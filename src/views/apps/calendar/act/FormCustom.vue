2f02d744ce28011cf58c29fdcd1461022182e7e1
uzz99 <yj.lee991228@gmail.com>
2024-10-17 15:24:11 +0900

    refact: #44 import 이름 변경

--- src/views/apps/calendar/act/FormCustom.vue
+++ src/views/apps/calendar/act/FormCustom.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-// common components
 import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
 import UiParentCard from '@/components/shared/UiParentCard.vue';
 import UiChildCard from '@/components/shared/UiChildCard.vue';
-import ActViewCopy from './ActView.vue'
-// theme breadcrumb
+import ActView from './ActView.vue'
+
 const page = ref({ title: 'Form Layouts' });
 const breadcrumbs = ref([
     {
@@ -27,7 +26,7 @@ const breadcrumbs = ref([
   <v-row>
         <v-col cols="12" class="mb-12">
             <UiParentCard title="영업 활동 등록">
-                <ActViewCopy/>
+                <ActView/>
             </UiParentCard>
         </v-col>
   </v-row>           
