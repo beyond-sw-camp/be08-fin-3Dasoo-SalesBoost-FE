46121d5c8363e6d075e6e9c4356e7cb50c0512fe
uzz99 <yj.lee991228@gmail.com>
2024-10-17 12:18:40 +0900

    feat: #44 영업활동 리스트 구현

---
+++ src/views/apps/calendar/act/ActList.vue
@@ -0,0 +1,179 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import { ref, onMounted } from 'vue';
+import { EditIcon, PointFilledIcon, TrashIcon } from 'vue-tabler-icons';
+import UiParentCard from '@/components/shared/UiParentCard.vue';
+import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
+import baseApi from '@/api/baseapi';
+import api from '@/api/axiosinterceptor';
+
+const page = ref({ title: '영업활동 목록' });
+const breadcrumbs = ref([
+{
+	text: '영업도구',
+	disabled: false,
+	to: '/'
+},
+{
+	text: '영업활동',
+	disabled: true,
+	to: ''
+	},
+]);
+
+const search = ref();
+const dialog = ref(false);
+const dialogDelete = ref(false);
+const headers = ref([
+  { title: '활동명', key: 'name' },
+  { title: '분류', key: 'purpose' },
+  { title: '활동 일자', key: 'actDate' },
+  { title: '완료여부', key: 'completeYn' },
+  { title: '', key: 'actions', sortable: false }
+]);
+
+const actList = ref([]);
+
+
+
+async function initialize() {
+try {
+  console.log('초기화!')
+  const response = await api.get('/acts');
+  console.log('response!',response)
+  actList.value = response.data.result;
+  } catch (error) {
+  console.error(error);
+  }
+}
+
+// TODO: 수정 삭제 추후 구현
+
+// const editedIndex = ref(-1);
+// const editedItem = ref({});
+// const defaultItem = ref({});
+// function editItem(item) {
+//   editedIndex.value = actList.value.indexOf(item);
+//   editedItem.value = Object.assign({}, item);
+//   dialog.value = true;
+// }
+
+// function deleteItem(item) {
+//   editedIndex.value = actList.value.indexOf(item);
+//   editedItem.value = Object.assign({}, item);
+//   dialogDelete.value = true;
+// }
+
+// function deleteItemConfirm() {
+//   actList.value.splice(editedIndex.value, 1);
+// closeDelete();
+// }
+
+// function close() {
+//   dialog.value = false;
+//   editedItem.value = Object.assign({}, defaultItem.value);
+//   editedIndex.value = -1;
+// }
+
+// function closeDelete() {
+//   dialogDelete.value = false;
+//   editedItem.value = Object.assign({}, defaultItem.value);
+//   editedIndex.value = -1;
+// }
+
+const router = useRouter();
+
+function goToAddAct() {
+  router.push('/apps/act');
+}
+
+function goToActDetails(actNo) {
+  router.push({ name: 'FormCustom', params: { actNo } });
+}
+
+function resetSearch() {
+  search.value = '';
+  initialize();
+}
+
+onMounted(() => {
+  initialize();
+});
+</script>
+<template>
+  <BaseBreadcrumb :title="page.title" class="" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>
+  <v-row>
+    <v-col cols="12">
+      <UiParentCard title="Act List">
+        <v-data-table
+          class="rounded-md datatabels actlist"
+          :headers="headers"
+          :items="actList"
+          v-model:search="search"
+          items-per-page="5"
+          item-value="name"
+          color="primary"
+          show-select
+        >
+          <template v-slot:item.name="{ item }">
+            <h6 class="text-h6 cursor-pointer" @click="goToActDetails(item.actNo)">{{ item.name }}</h6>
+          </template>
+
+          <template v-slot:item.purpose="{ item }">
+            <p>{{ item.purpose }}</p>
+          </template>
+
+          <template v-slot:item.actDate="{ item }">
+            <p>{{ item.actDate }}</p>
+          </template>
+
+          <template v-slot:item.completeYn="{ item }">
+            <div class="d-flex gap-2 align-center">
+              <PointFilledIcon v-if="item.completeYn === 'Y'" class="text-success" />
+              <PointFilledIcon v-else class="text-error" />
+              {{ item.completeYn === 'Y' ? '완료' : '미완료' }}
+            </div>
+          </template>
+
+          <template v-slot:item.actions="{ item }">
+            <div class="d-flex gap-2">
+              <EditIcon
+                height="20"
+                width="20"
+                class="text-primary cursor-pointer"
+                size="small"
+                @click="editItem(item)"
+              />
+              <TrashIcon
+                height="20"
+                width="20"
+                class="text-error cursor-pointer"
+                size="small"
+                @click="deleteItem(item)"
+              />
+            </div>
+          </template>
+
+          <template v-slot:top>
+            <v-toolbar class="bg-surface" flat>
+              <v-text-field
+                v-model="search"
+                append-inner-icon="mdi-magnify"
+                label="Search"
+                single-line
+                hide-details
+                class="mb-md-0 mb-3"
+              />
+              <v-spacer></v-spacer>
+              <v-btn color="primary" @click="goToAddAct">Add New</v-btn>
+            </v-toolbar>
+          </template>
+
+          <template v-slot:no-data>
+            <v-btn color="primary" @click="resetSearch"> Reset </v-btn>
+          </template>
+        </v-data-table>
+      </UiParentCard>
+    </v-col>
+  </v-row>
+</template>
