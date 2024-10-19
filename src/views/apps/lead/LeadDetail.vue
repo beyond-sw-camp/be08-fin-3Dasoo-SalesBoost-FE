ad30f62e9a527f58cae43121c577aabf9f33e959
mabem95 <jangdo95@gmail.com>
2024-10-19 10:24:35 +0900

    feat: #76 영업기회 관련 페이지 개발

---
+++ src/views/apps/lead/LeadDetail.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+
+import DetailForm from '@/components/lead/DetailForm.vue';
+</script>
+
+<template>
+    <v-row>
+        <v-col cols="12">
+            <DetailForm />
+        </v-col>
+    </v-row>
+</template>
