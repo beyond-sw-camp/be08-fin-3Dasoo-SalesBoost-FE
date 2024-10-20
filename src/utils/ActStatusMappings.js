22b1a2613a95f495da0cf55094e4f0deccba7287
uzz99 <yj.lee991228@gmail.com>
2024-10-21 01:34:36 +0900

    feat: #78 영업기회 연결

--- src/utils/ActStatusMappings.js
+++ src/utils/ActStatusMappings.js
@@ -1,19 +1,17 @@
 export const reverseActStatus = {
-  'MEETING': '고객 미팅',
-  'PRODUCT_INTRO': '제품 소개',
-  'NEGOTIATION': '협상',
-  'CONTRACT': '계약',
-  'ESTIMATE': '견적',
-  'PROPOSAL': '제안'
+  'PHONE': '전화',
+  'VISIT': '방문',
+  'EMAIL': '이메일',
+  'ONLINE': '온라인',
+  'OTHER': '기타',
 };
 
 export const actStatus = {
-  '고객 미팅': 'MEETING',
-  '제품 소개': 'PRODUCT_INTRO',
-  '협상': 'NEGOTIATION',
-  '계약': 'CONTRACT',
-  '견적': 'ESTIMATE',
-  '제안': 'PROPOSAL'
+  '전화': 'PHONE',
+  '방문': 'VISIT',
+  '이메일': 'EMAIL',
+  '온라인': 'ONLINE',
+  '기타': 'OTHER',
 };
 
 export const categoryMapping = {
