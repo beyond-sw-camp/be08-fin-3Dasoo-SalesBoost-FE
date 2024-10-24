51c86bec83013632bfaebcf4d5d8d2bdaa833012
uzz99 <yj.lee991228@gmail.com>
2024-10-24 21:31:22 +0900

    refact: #114 헤더에 불필요한 항목 정리

--- src/layouts/full/horizontal-header/HorizontalHeader.vue
+++ src/layouts/full/horizontal-header/HorizontalHeader.vue
@@ -3,16 +3,13 @@ import { ref, watch, computed } from 'vue';
 import { useCustomizerStore } from '../../../stores/customizer';
 import { useEcomStore } from '@/stores/apps/eCommerce';
 // Icon Imports
-import { GridDotsIcon, LanguageIcon, SearchIcon, Menu2Icon, BellRingingIcon, ShoppingCartIcon } from 'vue-tabler-icons';
+import { Menu2Icon } from 'vue-tabler-icons';
 import Logo from '../logo/Logo.vue';
 import LogoIcon from '../logo/LogoIcon.vue';
 import RtlLogo from '../logo/RtlLogo.vue';
 // dropdown imports
-import LanguageDD from '../vertical-header/LanguageDD.vue';
 import NotificationDD from '../vertical-header/NotificationDD.vue';
 import ProfileDD from '../vertical-header/ProfileDD.vue';
-import Searchbar from '../vertical-header/Searchbar.vue';
-import ThemeToggler from '../vertical-header/ThemeToggler.vue';
 
 const customizer = useCustomizerStore();
 const showSearch = ref(false);
@@ -58,45 +55,13 @@ const getCart = computed(() => {
                 <Menu2Icon size="25" />
             </v-btn>
 
-            <!-- ------------------------------------------------>
-            <!-- Search part -->
-            <!-- ------------------------------------------------>
-
-            <Searchbar />
-
-            <!---/Search part -->
             <v-spacer />
-            <!-- ---------------------------------------------- -->
-            <!---right part -->
-            <!-- ---------------------------------------------- -->
 
-            <!-- ---------------------------------------------- -->
-            <!-- translate -->
-            <!-- ---------------------------------------------- -->
-            <div class="me-1">
-                <ThemeToggler />
-            </div>
-            <LanguageDD />
 
-            <!-- ---------------------------------------------- -->
             <!-- Notification -->
-            <!-- ---------------------------------------------- -->
-
             <NotificationDD />
 
-            <!-- ---------------------------------------------- -->
-            <!-- ShoppingCart -->
-            <!-- ---------------------------------------------- -->
-            <v-btn icon variant="text" color="primary" to="/ecommerce/checkout">
-                <v-badge color="error" :content="getCart?.length">
-                    <!-- <ShoppingCartIcon stroke-width="1.5" size="22" /> -->
-                    <v-icon size="22">mdi-cart</v-icon>
-                </v-badge>
-            </v-btn>
-
-            <!-- ---------------------------------------------- -->
             <!-- User Profile -->
-            <!-- ---------------------------------------------- -->
             <div class="ml-3">
                 <ProfileDD />
             </div>
